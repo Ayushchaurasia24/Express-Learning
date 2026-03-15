91d9bc022eb8e48ed04cb1e8618d228e5df79355
Ayushchaurasia24 <chaurasiakd9425@gmail.com>
2026-03-15 19:01:05 +0530

    Added Express routes for orders and users

--- server.js
+++ server.js
@@ -1,52 +1,21 @@
 const express = require("express");
+
 const app = express();
-const PORT = 3000;
 
-// Custom Middleware
-function addUser(req, res, next) {
-    req.user = "Guest";   
-    next();
-}
-
-// Route using middleware
-app.get("/welcome", addUser, (req, res) => {
-    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+app.get("/orders", (req,res)=>{
+    res.send("Here list of all orders");
+});
+app.post("/orders", (req, res)=>{
+    res.send("new order created");
 });
 
-app.listen(PORT, () => {
-    console.log("Server running on port 3000");
+app.get("/users", (req,res)=>{
+    res.send("Here is list of all users")
+});
+app.post("/users", (req, res) => {
+  res.send("new user added");
 });
 
-
-
-// console.log("INDEX FILE RUNNING");
-// const express = require("express");
-// const app = express();
-// const PORT = 3000;
-
-// app.use((req,res,next)=>{
-//     console.log("Authentication called");
-//     next();
-// });
-
-// app.get("/", (req,res)=>{
-//     res.send("<h1>Server is working</h1>");
-// });
-
-// app.get("/library-2", (req,res,next)=>{
-//     console.log("Book recommendation");
-//     next();
-// }, (req,res)=>{
-//     res.send("<h1>In Library-2</h1>");
-// });
-
-// app.get("/library-3", (req,res,next)=>{
-//     console.log("For researcher only");
-//     next();
-// }, (req,res)=>{
-//     res.send("<h1>In Library-3</h1>");
-// });
-
-// app.listen(PORT, ()=>{
-//     console.log("Server running on port 3000");
-// });
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
