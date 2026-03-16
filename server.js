9134ea137ff015c6679538ad3e6ad90dded97928
Ayushchaurasia24 <chaurasiakd9425@gmail.com>
2026-03-16 15:19:26 +0530

    Implemented dynamic route using route params and query params

--- server.js
+++ server.js
@@ -2,38 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.get("/welcome/:username", (req, res)=>{
+    const username = req.params.username;
+    const role = req.query.role;
 
-// GET /products
-app.get("/products", (req, res) => {
-  res.send("Here is the list of all products.");
+    res.send(`<h1>Welcome ${username} your role is ${role}.</h1>`);
 });
 
-
-// POST /products
-app.post("/products", (req, res) => {
-  res.send("A new product has been added.");
-});
-
-
-// GET /categories
-app.get("/categories", (req, res) => {
-  res.send("Here is the list of all categories.");
-});
-
-
-// POST /categories
-app.post("/categories", (req, res) => {
-  res.send("A new category has been created.");
-});
-
-
-// Handle Unknown Routes (404)
-app.use((req, res) => {
-  res.status(404).send("<h1>404 - Page Not Found</h1>");
-});
-
-
-// Start Server
-app.listen(4000, () => {
-  console.log("Server is running on http://localhost:4000");
-});
+app.listen(3000, ()=>{
+    console.log("server runing http://localhost:3000");
+})
