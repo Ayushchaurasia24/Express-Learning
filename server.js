1ace6220ff4fda29828a64106de88b147609905d
Ayushchaurasia24 <chaurasiakd9425@gmail.com>
2026-03-16 15:59:52 +0530

    Implemented logging middleware and routest

--- server.js
+++ server.js
@@ -2,13 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.get("/welcome/:username", (req, res)=>{
-    const username = req.params.username;
-    const role = req.query.role;
-
-    res.send(`<h1>Welcome ${username} your role is ${role}.</h1>`);
+app.use((req,res,next) => {
+    console.log(`${req.method} request made to ${req.url}`);
+    next();
 });
 
-app.listen(3000, ()=>{
-    console.log("server runing http://localhost:3000");
+//Product
+app.get("/products", (req,res) => {
+    res.send("Here is the list of all products.");
+});
+app.post("/products", (req,res)=>{
+    res.send("Here product added");
+});
+
+//Categories
+app.get("/categories", (req,res)=>{
+    res.send("Here is list of all categ");
+});
+app.post("/categories" , (req,res)=>{
+    res.send("A new catg created");
+});
+
+
+app.listen(4000, ()=>{
+    console.log("server runing http://localhost:4000");
 })
